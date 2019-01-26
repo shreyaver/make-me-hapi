d43b64fa215b408e3ef34b1557627b6932e68be0
Shreya Verma <svshreya97@gmail.com>
2019-01-26 11:04:32 +0530

    Added more tests

--- 2-name.test.js
+++ 2-name.test.js
@@ -1,18 +1,58 @@
 const server = require('./2-name.js');
 
 describe('server', () => {
-  const options = {
-    method: 'GET',
-    url: '/Shreya',
-  };
-  it('should return "Hello <name>" string', async () => {
+  it('should return "Hello <first path parameter>" string', async () => {
+    const options = {
+      method: 'GET',
+      url: '/Shreya',
+    };
     const serverResponse = await server.inject(options);
     console.log(serverResponse.result);
     expect(serverResponse.result).toEqual('Hello Shreya');
   });
-  it('should not return anything other than "Hello <name>" string', async () => {
+  it('should not return anything other than "Hello <first path parameter>" string', async () => {
+    const options = {
+      method: 'GET',
+      url: '/Shreya',
+    };
     const serverResponse = await server.inject(options);
     console.log(serverResponse.result);
-    expect(serverResponse.result).not.toEqual('abc');
+    expect(serverResponse.result).not.toEqual('Hello shreya');
+  });
+  it('should return "Hello <first path parameter>" with number', async () => {
+    const options = {
+      method: 'GET',
+      url: '/123',
+    };
+    const serverResponse = await server.inject(options);
+    console.log(serverResponse.result);
+    expect(serverResponse.result).toEqual('Hello 123');
+  });
+  it('should return "Hello <first path parameter>" HTML safe string with space', async () => {
+    const options = {
+      method: 'GET',
+      url: '/Shreya Verma',
+    };
+    const serverResponse = await server.inject(options);
+    console.log(serverResponse.result);
+    expect(serverResponse.result).toEqual('Hello Shreya%20Verma');
+  });
+  it('should return "Hello <first path parameter>" HTML safe string with space and numbers', async () => {
+    const options = {
+      method: 'GET',
+      url: '/12 3',
+    };
+    const serverResponse = await server.inject(options);
+    console.log(serverResponse.result);
+    expect(serverResponse.result).toEqual('Hello 12%203');
+  });
+  it('should return "Hello <first path parameter>" HTML safe string with &', async () => {
+    const options = {
+      method: 'GET',
+      url: '/12&3',
+    };
+    const serverResponse = await server.inject(options);
+    console.log(serverResponse.result);
+    expect(serverResponse.result).toEqual('Hello 12%263');
   });
 });
