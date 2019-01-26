db8f2985b0ad09e9f147151496d3b9418a1bebb0
Shreya Verma <svshreya97@gmail.com>
2019-01-26 11:03:29 +0530

    Handled invalid paths and added more tests

--- 1-hello-hapi.js
+++ 1-hello-hapi.js
@@ -1,17 +1,15 @@
 const Hapi = require('hapi');
+const routes = require('./routes/1-hello-hapi-route.js');
 
 const server = Hapi.Server({
   host: 'localhost',
   port: Number(process.argv[2]) || 8080,
 });
-server.route({
-  path: '/',
-  method: 'GET',
-  handler: (request, h) => 'Hello hapi',
-});
+server.route(routes);
 if (!module.parent) {
   const startListening = async () => {
     await server.start();
+    console.log(server.info);
     console.log('Server running at:', server.info.uri);
   };
   startListening();
--- 1-hello-hapi.test.js
+++ 1-hello-hapi.test.js
@@ -1,16 +1,54 @@
 const server = require('./1-hello-hapi.js');
 
-describe('server', () => {
+describe('server at path "/"', () => {
   const options = {
     method: 'GET',
     url: '/',
   };
-  it('should return hello hapi string', async () => {
+  it('should return "Hello hapi" string', async () => {
     const serverResponse = await server.inject(options);
     expect(serverResponse.result).toEqual('Hello hapi');
   });
-  it('should return hello hapi string', async () => {
+  it('should not return anything other than "Hello hapi" string', async () => {
     const serverResponse = await server.inject(options);
-    expect(serverResponse.result).not.toEqual('abc');
+    expect(serverResponse.result).not.toEqual('hello hapi');
+  });
+  it('should return status code 200 for "OK"', async () => {
+    const serverResponse = await server.inject(options);
+    expect(serverResponse.statusCode).toEqual(200);
+  });
+});
+describe('server at path other than "/"', () => {
+  it('should return "You have reached unexplored territory" string for "/<string>"', async () => {
+    const options = {
+      method: 'GET',
+      url: '/abc',
+    };
+    const serverResponse = await server.inject(options);
+    expect(serverResponse.result).toEqual('You have reached unexplored territory');
+  });
+  it('should return "You have reached unexplored territory" string for "/ <string>"', async () => {
+    const options = {
+      method: 'GET',
+      url: '/ abc',
+    };
+    const serverResponse = await server.inject(options);
+    expect(serverResponse.result).toEqual('You have reached unexplored territory');
+  });
+  it('should return "You have reached unexplored territory" string for "/<string>/<string>"', async () => {
+    const options = {
+      method: 'GET',
+      url: '/abc/def',
+    };
+    const serverResponse = await server.inject(options);
+    expect(serverResponse.result).toEqual('You have reached unexplored territory');
+  });
+  it('should return status code 404 for "Not Found"', async () => {
+    const options = {
+      method: 'GET',
+      url: '/abc',
+    };
+    const serverResponse = await server.inject(options);
+    expect(serverResponse.statusCode).toEqual(404);
   });
 });
