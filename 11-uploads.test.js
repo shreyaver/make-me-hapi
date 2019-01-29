3f31908c8a7b37471d2b9c02b03218e8a2e6852f
Shreya Verma <svshreya97@gmail.com>
2019-01-29 10:28:27 +0530

    Added test for upload

---
+++ 11-uploads.test.js
@@ -0,0 +1,35 @@
+const FormData = require('form-data');
+const fs = require('fs');
+const streamToPromise = require('stream-to-promise');
+const server = require('./11-uploads');
+
+describe('server at path "/login" with payload {isGuest: true, accessToken: <string>}', () => {
+  it('should return "login successful"', async () => {
+    const fileStream = fs.createReadStream('fileToUpload.txt');
+    const form = new FormData();
+    form.append('description', 'This file will be uploaded');
+    form.append('file', fileStream);
+    let options = {};
+    await streamToPromise(form).then((payloadF) => {
+      options = {
+        method: 'POST',
+        url: '/upload',
+        payload: payloadF,
+        headers: form.getHeaders(),
+      };
+    });
+    const serverResponse = await server.inject(options);
+    console.log(serverResponse.result);
+    expect(serverResponse.result).toEqual(JSON.stringify({
+      description: 'This file will be uploaded',
+      file: {
+        data: 'a\nb\nc',
+        filename: 'fileToUpload.txt',
+        headers: {
+          'content-disposition': 'form-data; name="file"; filename="fileToUpload.txt"',
+          'content-type': 'text/plain',
+        },
+      },
+    }));
+  });
+});
